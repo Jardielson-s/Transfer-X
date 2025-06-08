e4cf730dbfccf392bc01d8b61bf8145b474bd7c0
Jardielson <jardielsonbio@gmail.com>
2025-06-07 22:40:26 -0300

    fix: comment externalUserId

--- src/application/users/users.application.ts
+++ src/application/users/users.application.ts
@@ -124,7 +124,7 @@ export class UserApplicationFactory {
           //   ...input,
           //   cpfCnpj: input.ein,
           // });
-          input.externalUserId = new bson.ObjectId().toString();
+          // input.externalUserId = userExternalId;
           input.externalApplication = true;
           upsert.push(input);
         }
